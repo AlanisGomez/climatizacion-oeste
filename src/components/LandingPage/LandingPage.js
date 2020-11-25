59753a5422678f1204b9a7f48d35fddf6631cd10
Alanis Gomez <agomez.aper@gmail.com>
2020-11-25 10:52:34 -0300

    Se agrega nueva imagen y se configura el formulario de contacto

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -14,27 +14,31 @@ export default {
     data () {
       return {
         user: {
-          name: "",
-          email: "",
-          phone: "",
-          msg: ""
+          name: '',
+          email: '',
+          phone: '',
+          msg: '',
         },
         loader: false
     }
   },
   methods: {
-      registerUser() {
-          this.loader = true
-          http
-            .post('http://localhost:4000/send-email', this.user)
-            .then(res => {
-              console.log(res)
-              alert("Consulta enviada exitosamente");
-              this.loader = false
-            }).catch (err => {
-              alert("Ups! Algo salió mal. Intentalo nuevamente o escribinos directamente a contacto@climatizacionoeste.com");
-              this.loader = false
-          })
-      }
-  },
+    sendEmail() {
+      this.loader = true
+      http
+        .post('http://localhost:5000/send-email', this.user)
+        .then(res => {
+          console.log(res)
+          alert("Consulta enviada exitosamente")
+          this.user.name = ''
+          this.user.email = ''
+          this.user.phone = ''
+          this.user.msg = ''
+          this.loader=  false
+        }).catch(err => {
+          alert("Ups! Algo salió mal. Intentalo nuevamente o escribinos directamente a contacto@climatizacionoeste.com");
+          this.loader = false
+        })
+    },
+  }
 }
